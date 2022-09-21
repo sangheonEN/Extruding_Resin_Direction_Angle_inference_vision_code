8ca32449dd76a40862ac053d1341219077023713
sangheonEN <69128174+sangheonEN@users.noreply.github.com>
2022-09-21 14:06:40 +0900

    side_angle_extract
    -

--- image_task/image_preprocessing/extract_angle.py
+++ image_task/image_preprocessing/extract_angle.py
@@ -295,6 +295,128 @@ def front_pca_angle_direction_change(img, img_path, angle_list):
     return left_angle, right_angle
 
 
+def side_pca_angle_direction_change(img, img_path, angle_list):
+    # 이미지 좌표 y좌표 평행이동 및 y축 반전
+
+    img_label_index = np.unique(img)
+
+    rgb = visualization(img)
+
+    exflusion = np.where(img == img_label_index[1])
+    left_curve = np.where(img == img_label_index[2])
+    right_curve = np.where(img == img_label_index[3])
+
+    left_curve_h = left_curve[0]
+    # y 축 기준으로 flip
+    left_curve_h *= -1
+    left_curve_w = left_curve[1]
+
+    right_curve_h = right_curve[0]
+    # y 축 기준으로 flip
+    right_curve_h *= -1
+    right_curve_w = right_curve[1]
+
+    # left, right 좌표 array 데이터
+    left_curve_array = np.array([left_curve_w, left_curve_h], dtype=np.float64).transpose(1, 0)
+    right_curve_array = np.array([right_curve_w, right_curve_h], dtype=np.float64).transpose(1, 0)
+
+    # pca
+    mean = np.empty((0))
+    mean_left, eigenvectors_left, eigenvalues_left = cv2.PCACompute2(left_curve_array, mean)
+    mean_right, eigenvectors_right, eigenvalues_right = cv2.PCACompute2(right_curve_array, mean)
+
+    # 고유벡터와 x축 사이 angle 추출
+    left_angle = atan2(eigenvectors_left[0, 1], eigenvectors_left[0, 0])
+    right_angle = atan2(eigenvectors_right[0, 1], eigenvectors_right[0, 0])
+    left_angle = int(np.rad2deg(left_angle))
+    right_angle = int(np.rad2deg(right_angle))
+
+
+    left_cntr = (int(mean_left[0, 0]), int(mean_left[0, 1]))
+    right_cntr = (int(mean_right[0, 0]), int(mean_right[0, 1]))
+
+
+    cv2.circle(rgb, left_cntr, 3, (255, 0, 255), 2)
+    cv2.circle(rgb, right_cntr, 3, (255, 0, 255), 2)
+    p1_left = (left_cntr[0] + 0.02 * eigenvectors_left[0, 0] * eigenvalues_left[0, 0], left_cntr[1] + 0.02 * eigenvectors_left[0, 1] * eigenvalues_left[0, 0])
+    # p2_left = (left_cntr[0] - 0.02 * eigenvectors_left[1, 0] * eigenvalues_left[1, 0], left_cntr[1] - 0.02 * eigenvectors_left[1, 1] * eigenvalues_left[1, 0])
+
+    p1_right = (right_cntr[0] + 0.02 * eigenvectors_right[0, 0] * eigenvalues_right[0, 0],
+          right_cntr[1] + 0.02 * eigenvectors_right[0, 1] * eigenvalues_right[0, 0])
+    # p2_right = (left_cntr[0] - 0.02 * eigenvectors_right[1, 0] * eigenvalues_right[1, 0],
+    #       right_cntr[1] - 0.02 * eigenvectors_right[1, 1] * eigenvalues_right[1, 0])
+
+    left_angle = angle_(left_cntr, p1_left)
+    # drawAxis(rgb, left_cntr, p2_left, (255, 255, 0), 10)
+
+    right_angle = angle_(right_cntr, p1_right)
+    # drawAxis(rgb, right_cntr, p2_right, (255, 255, 0), 10)
+
+    left_angle = np.rad2deg(left_angle)
+    right_angle = np.rad2deg(right_angle)
+
+    change_direction_hyperparameters = 0 # direction change = 180, not change = 0
+
+    final_angle = (left_angle + right_angle)/2 + change_direction_hyperparameters
+
+    if left_angle < 0:
+        left_angle = left_angle + 360
+
+    if right_angle < 0:
+        right_angle = right_angle + 360
+
+    # cv2.putText(rgb, f"left_angle: {left_angle}", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+    # cv2.putText(rgb, f"right_angle: {right_angle}", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+
+
+    mean_exflusion_h = int(np.mean(exflusion[0]))
+    mean_exflusion_w = int(np.mean(exflusion[1]))
+
+    y_rotated = mean_exflusion_h + int(np.sin(np.pi / -180 * final_angle)*600)
+    x_rotated = mean_exflusion_w + int(np.cos(np.pi / -180 * final_angle)*600)
+
+    # 기존 좌표와 이미지 끝에 교차된 점 까지를 이은 직선을 그립니다.
+    cv2.line(rgb, (mean_exflusion_w, mean_exflusion_h), (x_rotated, y_rotated), (0, 255, 0), 2, cv2.LINE_AA)
+
+    angle = atan2(float(mean_exflusion_h - y_rotated), float(mean_exflusion_w - x_rotated)) # angle in radians
+
+    # create the arrow hooks
+    mean_exflusion_w = x_rotated + 9 * cos(angle + pi / 4)
+    mean_exflusion_h = y_rotated + 9 * sin(angle + pi / 4)
+    cv2.line(rgb, (int(mean_exflusion_w), int(mean_exflusion_h)), (int(x_rotated), int(y_rotated)), (0, 255, 0), 5, cv2.LINE_AA)
+    mean_exflusion_w = x_rotated + 9 * cos(angle - pi / 4)
+    mean_exflusion_h = y_rotated + 9 * sin(angle - pi / 4)
+    cv2.line(rgb, (int(mean_exflusion_w), int(mean_exflusion_h)), (int(x_rotated), int(y_rotated)), (0, 255, 0), 5, cv2.LINE_AA)
+
+
+    # # create the arrow hooks
+    # p[0] = q[0] + 9 * cos(angle + pi / 4)
+    # p[1] = q[1] + 9 * sin(angle + pi / 4)
+    # cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+    # p[0] = q[0] + 9 * cos(angle - pi / 4)
+    # p[1] = q[1] + 9 * sin(angle - pi / 4)
+    # cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv.LINE_AA)
+
+    if final_angle < 0:
+        final_angle = final_angle + 360
+
+    cv2.putText(rgb, f"final_angle: {round(final_angle, 2)}", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+    cv2.imwrite(f"./pca_angle/side/{img_path}", rgb)
+
+    angle_list.append(final_angle)
+
+    # while (1):
+    #     cv2.imshow('image', rgb)
+    #     k = cv2.waitKey(1) & 0xFF
+    #
+    #     if k == 27:
+    #         break
+    #
+    # cv2.destroyAllWindows()
+
+    return left_angle, right_angle
+
+
 
 def side_pca_angle(img, img_path):
 
@@ -469,7 +591,7 @@ if __name__ == "__main__":
         # front
         # left_angle, right_angle = front_pca_angle_case0234(img, image_path)
 
-        left_angle, right_angle = front_pca_angle_direction_change(img, image_path, angle_list)
+        left_angle, right_angle = side_pca_angle_direction_change(img, image_path, angle_list)
 
         # print(f"left_angle: {left_angle}\n")
         # print(f"right_angle: {right_angle}\n")
