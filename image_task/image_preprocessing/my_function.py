1c742e9ab765df7d614da435930eb40551a9147f
sangheonEN <69128174+sangheonEN@users.noreply.github.com>
2022-11-22 18:54:01 +0900

    Function parameter review

--- image_task/image_preprocessing/my_function.py
+++ image_task/image_preprocessing/my_function.py
@@ -3,26 +3,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.arange(-1., 2., 0.01, dtype=float)
-y = 1/np.exp(x) + 1
+# y = 1/np.exp(x) + 1
+y = ((1/np.exp(x)) + 1) * (1- (x/(np.exp(1)+1)))
 
 y0 = np.arange(-1., 5., 0.01, dtype=float)
 x0 = np.zeros((y0.shape))
 x1 = np.ones((y0.shape))
 x2 = 1
-y2 = 1/np.exp(x2) + 1
+# y2 = 1/np.exp(x2) + 1
+y2 = ((1/np.exp(x2)) + 1) * (1- (x2/(np.exp(1)+1)))
 
 x3 = 0
-y3 = 1/np.exp(x3) + 1
+# y3 = 1/np.exp(x3) + 1
+y3 = ((1/np.exp(x3)) + 1) * (1- (x3/(np.exp(1)+1)))
 
 x4 = np.arange(-3., 0, 0.01, dtype=float)
+# y4 = np.ones((x4.shape)) * 2
 y4 = np.ones((x4.shape)) * 2
 x5 = np.arange(-3., 1., 0.01, dtype=float)
-y5 = np.ones((x5.shape)) * 1.37
+# y5 = np.ones((x5.shape)) * 1.37
+y5 = np.ones((x5.shape)) * 1
 
 
 fig, ax = plt.subplots(figsize=(5, 5))
 
-ax.set_xlim([0, 1.5])
+ax.set_xlim([0, 3])
 ax.set_ylim([0, 3])
 
 ax.plot(x, y, c='black')
